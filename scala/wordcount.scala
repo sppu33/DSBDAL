a734c5cb46a6a8289403d23f2a55d737072b1a9f
sppu33 <138193237+sppu33@users.noreply.github.com>
2025-04-28 17:39:48 +0530

    Add files via upload

---
+++ scala/wordcount.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.SparkSession
+
+import org.apache.spark.sql.functions._
+
+object WordCount{
+    def main(arg: Array[String]): Unit = {
+        val spark = SparkSession.builder.appName("wordCount").master("local[*]").getOrCreate()
+
+        val lines = spark.readStream.format("socket").option("host", "localhost").option("port", 9999).load()
+
+        import spark.implicits._
+
+        val words = lines.as[String].flatMap(_.split(" "))
+
+        val wordCount = words.groupBy("value").count()
+
+        val query = wordCount.writeStream.outputMode("complete").format("console").start()
+
+        query.awaitTermination()
+    }
+}
